f28e4772a5b7d2bcb37715b1bb4d295775e8e73b
Muhammadamin Avazov <augustin.coder@gmail.com>
2026-03-25 01:53:36 +0500

    Change Telegram bot token
    Updated the BOT_TOKEN for the Telegram bot.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 from pathlib import Path
 
 # Telegram Bot Token - BotFather'dan olingan tokeningiz
-BOT_TOKEN = "8764719096:AAEiFsn-vO6zM5YHaooNv24Pzu2DDZumlOI"
+BOT_TOKEN = "8764719096:AAHqT0lrIM9o_uE23ZtqGCWe7R8jzJYp0e4"
 
 # Data storage
 DATA_DIR = Path(__file__).parent / "data"
